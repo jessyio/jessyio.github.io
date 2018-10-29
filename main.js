0df573d3295edce09d2d3101fbcd6dad41d1fc4e
kink jun un <jessy.huang@watsonuniversity.org>
2018-10-28 18:19:34 -0600

    initialize

---
+++ main.js
@@ -0,0 +1,99 @@
+$( document ).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+
+});
+
+function fetchIssues () {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesList = document.getElementById('issuesList');
+
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    var id = issues[i].id;
+    var desc = issues[i].description;
+    var severity = issues[i].severity;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+
+    issuesList.innerHTML +=   '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
+                              '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
+                              '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+                              '</div>';
+
+  }
+}
+/*document.getElementById('issueInputForm').addEventListener('submit', hello);
+function hello(inputName1, inputName2) {
+    alert("hello " + inputName1 + inputName2);
+}*/
+
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+function saveIssue(e) {
+  var issueId = chance.guid();
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueStatus = 'Open';
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+  //alert(issue.id);
+
+  if (localStorage.getItem('issues') === null) {
+    var issues = [];
+    //alert("issues");
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    //localStorage.removeItem('issues');
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    //alert("2issues");
+    //e.preventDefault();
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+
+  document.getElementById('issueInputForm').reset();
+
+  fetchIssues();
+
+  e.preventDefault();
+}
+
+function setStatusClosed (id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for(var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = "Closed";
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function deleteIssue (id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for(var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i,1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
